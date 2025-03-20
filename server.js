0e2e6e12a2c3b7f201254da568a24ac1b61030c2
JohnNg-GA <john.q.ng@gmail.com>
2025-03-20 15:10:44 -0700

    successfully converted project-one into an express file that is now loadable on AWS.

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.listen(3002, () => {
 })
 
 app.get('/', (req, res) => {
+    res.render('main.ejs')
+})
+
+app.get('/gallery', (req, res) => {
     res.render('gallery.ejs')
 })
 
+app.get('/news', (req, res) => {
+    res.render('news.ejs')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.ejs')
+})
